6cc57c1e37fb23790dcfc1fbcae1c5136d658f90
LeoIta <leonardolorusso83@gmail.com>
2021-01-10 21:32:56 +0100

    use form data

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -10,6 +10,14 @@ def password(request):
     
     characters = list('abcdefghijklmnopqrstuvwxyz')
 
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+    if request.GET.get('special'):
+        characters.extend(list('~!@#$%^&*'))
+    
+    
     length = int(request.GET.get('length',default=12))
 
     my_password = ''
