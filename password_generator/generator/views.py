0e6529ae45672fc949ea9ac922a476a0666f7b68
LeoIta <leonardolorusso83@gmail.com>
2021-01-10 19:02:21 +0100

    set 2nd page

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Hello dear friend!")
+
+def mypage(request):
+    return HttpResponse("<h1>welcome to mypage!</h1>")
