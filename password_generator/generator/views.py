c2179572c791e08f1418dbb9d0bc740e6d3738cf
LeoIta <leonardolorusso83@gmail.com>
2021-01-10 20:04:02 +0100

    add dynamic value in render

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request,'generator/home.html')
+    return render(request,'generator/home.html',{'anykey' : 'anyvalue'})
 
 def mypage(request):
     return HttpResponse("<h1>welcome to mypage!</h1>")
