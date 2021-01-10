e099b4a9d5bfb2641d900232b5064e6ea57c0fbf
LeoIta <leonardolorusso83@gmail.com>
2021-01-10 21:15:23 +0100

    generate random small letters password

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 def home(request):
     return render(request,'generator/home.html')
 
 def password(request):
-    my_password = 'null'
+    
+    
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+
+    length = 10
+
+    my_password = ''
+
+    for char in range(length):
+        my_password += random.choice(characters)
+
     return  render(request,'generator/password.html',{'password':my_password})
