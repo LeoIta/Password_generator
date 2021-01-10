c973f2f6e17c425cfa11c0b4f4371280c55b6526
LeoIta <leonardolorusso83@gmail.com>
2021-01-10 20:01:10 +0100

    render vs HttpResponse

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello dear friend!")
+    return render(request,'generator/home.html')
 
 def mypage(request):
     return HttpResponse("<h1>welcome to mypage!</h1>")
